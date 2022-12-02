a3bc0d3d8c19b17178d248c5ebeb3f6275cc974a
Zen <99693461+VoiceOfZen@users.noreply.github.com>
2022-12-01 17:27:29 -0800

    4
    4

--- constants.py
+++ constants.py
@@ -1,8 +1,74 @@
-Y1 = 'year1'
-Y2 = 'year2'
-Y3 = 'year3'
-Y1S = 'year1spo'
-Y2S = 'year2spo'
-Y3S = 'year3spo'
-Y4 = 'year4'
+FACULTY_KEY = 'faculty'
+FORM_KEY = 'form'
 YEAR_KEY = 'year'
+GROUP_KEY = 'group'
+
+FACULTY_0 = 'faculty_of_IT'
+
+# forms
+FORM_0 = 'ochnaya'
+FORM_1 = 'ochno-zaochnaya_vikhodnogo_dnya'
+FORM_2 = 'zaochnaya_vikhodnogo_dnya'
+
+# years of FORM_0
+Y1_F0 = 'year_1_form_0'
+Y2_F0 = 'year_2_form_0'
+Y3_F0 = 'year_3_form_0'
+Y1S_F0 = 'year_1spo_form_0'
+Y2S_F0 = 'year_2spo_form_0'
+Y3S_F0 = 'year_3spo_form_0'
+Y4_F0 = 'year_4_form_0'
+
+# groups of Y1_F0 
+G0_Y1_F0 = 'group_ID 30.1/B-22'
+G1_Y1_F0 = 'group_ID 23.1/B-22'
+# groups of Y2_F0 
+G0_Y2_F0 = 'group_o. ID 23.1/B-21'
+G1_Y2_F0 = 'group_o. ID 23.2/B-21'
+G2_Y2_F0 = 'group_o. ID 23.3/B-21'
+G3_Y2_F0 = 'group_o. ID 23.4/B-21'
+G4_Y2_F0 = 'group_o. ID 30.1/B-21'
+# groups of Y3_F0 
+G0_Y3_F0 = 'group_ID 23.1/B1-20'
+G1_Y3_F0 = 'group_ID 23.1/B2-20'
+G2_Y3_F0 = 'group_ID 23.2/B2-20'
+# groups of Y1S_F0
+G0_Y1S_F0 = 'group_IDs 23.1/B-22'
+# groups of Y2S_F0
+G0_Y2S_F0 = 'group_o.IDs 23.1/B3-21'
+# groups of Y3S_F0
+G0_Y3S_F0 = 'group_IDs 23.1/B1-20'
+# groups of Y4_F0 
+G0_Y4_F0 = 'group_ID 23.1/B1-19'
+G1_Y4_F0 = 'group_ID 23.2/B1-19'
+G2_Y4_F0 = 'group_ID 23.1/B2-19'
+
+
+# years of FORM_1
+Y1_F1 = 'year_1_form_1'
+Y1S_F1 = 'year_1spo_form_1'
+Y2S_F1 = 'year_2spo_form_1'
+
+# groups of Y1_F1 
+G0_Y1_F1 = 'group_IVS 30.1/B-22'
+# groups of Y1S_F1 
+G0_Y1S_F1 = 'group_IVSs 30.1/B-22'
+# groups of Y2S_F1
+G0_Y2S_F1 = 'group_o.IVSs 23.1/B-21'
+G1_Y2S_F1 = 'group_o.IVSs 30.1/B-21'
+
+
+# years of FORM_2
+Y1S_F2 = 'year_1_form_2'
+Y3S_F2 = 'year_3_form_2'
+Y4S_F2 = 'year_4_form_2'
+
+# groups of Y1S_F2
+G0_Y1S_F2 = 'group_ISs 23.1/B-22'
+G1_Y1S_F2 = 'group_ISs 23.2/B-22'
+
+# groups of Y3S_F2
+G0_Y3S_F2 = 'group_ISs 23.1/B1-20'
+
+# groups of Y4S_F2
+G0_Y4S_F2 = 'group_ISs 23.1/B1-19'
