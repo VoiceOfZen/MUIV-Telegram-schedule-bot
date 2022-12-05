715dd433e7a6801d0c907d11435928bf12598554
Zen <99693461+VoiceOfZen@users.noreply.github.com>
2022-12-05 13:16:44 -0800

    10
    added back button

--- bot.py
+++ bot.py
@@ -1,13 +1,19 @@
 import telebot
-import config
 import json
-from constants import *
 
+import config
+
+from constants import *
 from telebot import types
 
 bot = telebot.TeleBot(config.TOKEN)
 with open('./users.txt') as fi:
     di = json.load(fi)
+
+
+def add_keyboard_back_button(keyboard):
+    back = types.InlineKeyboardButton(text='в начало', callback_data='back')
+    keyboard.add(back)
 
 
 @bot.message_handler(content_types=['text'])
@@ -45,6 +51,13 @@ def save_dict():
 def callback_query(call):
     # try:
     user_id = str(call.from_user.id)
+
+    print()
+
+    if call.data == 'back':
+        di[user_id] = {FACULTY_KEY: 0, FORM_KEY: 0, YEAR_KEY: 0, GROUP_KEY: 0}
+        save_dict()
+        choose_faculty_menu(user_id)
 
     FACULTIES = {FACULTY_0}
     if call.data in FACULTIES:  # call.data = callback data that we sent when pressed the button
@@ -95,6 +108,8 @@ def choose_faculty_menu(user_id):
     keyboard = types.InlineKeyboardMarkup(row_width=1)  # inline keyboard
     button = types.InlineKeyboardButton(text='IT Faculty', callback_data=FACULTY_0)
     keyboard.add(button)
+    add_keyboard_back_button(keyboard)
+
     question = 'Whats your Faculty?'
     bot.send_message(user_id, text=question, reply_markup=keyboard)
 
@@ -105,8 +120,9 @@ def choose_form_menu(user_id, faculty):
     button_och = types.InlineKeyboardButton(text='Ochnaya', callback_data=FORM_0)
     button_och_zao = types.InlineKeyboardButton(text='Ochno-zaochaya vihodnogo dnya', callback_data=FORM_1)
     button_zao = types.InlineKeyboardButton(text='Zaochnaya vikhodnogo dnya', callback_data=FORM_2)
-
     keyboard.add(button_och, button_och_zao, button_zao)
+    add_keyboard_back_button(keyboard)
+
     bot.send_message(user_id, text=question, reply_markup=keyboard)
 
 
@@ -117,7 +133,7 @@ def choose_group_and_check_year(user_id, year):
         group_0 = types.InlineKeyboardButton(text=G0_Y1_F0, callback_data=G0_Y1_F0)
         group_1 = types.InlineKeyboardButton(text=G1_Y1_F0, callback_data=G1_Y1_F0)
         keyboard.add(group_0, group_1)
-
+        add_keyboard_back_button(keyboard)
         bot.send_message(user_id, text=question, reply_markup=keyboard)
     elif year == Y2_F0:
         keyboard = types.InlineKeyboardMarkup(row_width=1)
@@ -127,6 +143,7 @@ def choose_group_and_check_year(user_id, year):
         group_3 = types.InlineKeyboardButton(text='о. ИД 23.4/Б-21', callback_data=G3_Y2_F0)
         group_4 = types.InlineKeyboardButton(text=G4_Y2_F0, callback_data=G4_Y2_F0)
         keyboard.add(group_0, group_1, group_2, group_3, group_4)
+        add_keyboard_back_button(keyboard)
 
         bot.send_message(user_id, text=question, reply_markup=keyboard)
     elif year == Y3_F0:
@@ -135,24 +152,28 @@ def choose_group_and_check_year(user_id, year):
         group_1 = types.InlineKeyboardButton(text=G1_Y3_F0, callback_data=G1_Y3_F0)
         group_2 = types.InlineKeyboardButton(text=G2_Y3_F0, callback_data=G2_Y3_F0)
         keyboard.add(group_0, group_1, group_2)
+        add_keyboard_back_button(keyboard)
 
         bot.send_message(user_id, text=question, reply_markup=keyboard)
     elif year == Y1S_F0:
         keyboard = types.InlineKeyboardMarkup(row_width=1)  # inline keyboard
         group_0 = types.InlineKeyboardButton(text=G0_Y1S_F0, callback_data=G0_Y1S_F0)
         keyboard.add(group_0)
+        add_keyboard_back_button(keyboard)
 
         bot.send_message(user_id, text=question, reply_markup=keyboard)
     elif year == Y2S_F0:
         keyboard = types.InlineKeyboardMarkup(row_width=1)  # inline keyboard
         group_0 = types.InlineKeyboardButton(text=G0_Y2S_F0, callback_data=G0_Y2S_F0)
         keyboard.add(group_0)
+        add_keyboard_back_button(keyboard)
 
         bot.send_message(user_id, text=question, reply_markup=keyboard)
     elif year == Y3S_F0:
         keyboard = types.InlineKeyboardMarkup(row_width=1)  # inline keyboard
         group_0 = types.InlineKeyboardButton(text=G0_Y3S_F0, callback_data=G0_Y3S_F0)
         keyboard.add(group_0)
+        add_keyboard_back_button(keyboard)
 
         bot.send_message(user_id, text=question, reply_markup=keyboard)
     elif year == Y4_F0:
@@ -161,6 +182,7 @@ def choose_group_and_check_year(user_id, year):
         group_1 = types.InlineKeyboardButton(text=G1_Y4_F0, callback_data=G1_Y4_F0)
         group_2 = types.InlineKeyboardButton(text=G2_Y4_F0, callback_data=G2_Y4_F0)
         keyboard.add(group_0, group_1, group_2)
+        add_keyboard_back_button(keyboard)
 
         bot.send_message(user_id, text=question, reply_markup=keyboard)
 
@@ -168,29 +190,46 @@ def choose_group_and_check_year(user_id, year):
         keyboard = types.InlineKeyboardMarkup(row_width=1)  # inline keyboard
         group_0 = types.InlineKeyboardButton(text=G0_Y1_F1, callback_data=G0_Y1_F1)
         keyboard.add(group_0)
+        add_keyboard_back_button(keyboard)
+
+        bot.send_message(user_id, text=question, reply_markup=keyboard)
     elif year == Y1S_F1:
         keyboard = types.InlineKeyboardMarkup(row_width=1)  # inline keyboard
         group_0 = types.InlineKeyboardButton(text=G0_Y1S_F1, callback_data=G0_Y1S_F1)
         keyboard.add(group_0)
+        add_keyboard_back_button(keyboard)
+
+        bot.send_message(user_id, text=question, reply_markup=keyboard)
     elif year == Y2S_F1:
         keyboard = types.InlineKeyboardMarkup(row_width=1)  # inline keyboard
         group_0 = types.InlineKeyboardButton(text=G0_Y2S_F1, callback_data=G0_Y2S_F1)
         group_1 = types.InlineKeyboardButton(text=G1_Y2S_F1, callback_data=G1_Y2S_F1)
         keyboard.add(group_0, group_1)
+        add_keyboard_back_button(keyboard)
 
+        bot.send_message(user_id, text=question, reply_markup=keyboard)
     elif year == Y1S_F2:
         keyboard = types.InlineKeyboardMarkup(row_width=1)  # inline keyboard
         group_0 = types.InlineKeyboardButton(text=G0_Y1S_F2, callback_data=G0_Y1S_F2)
         group_1 = types.InlineKeyboardButton(text=G1_Y2S_F1, callback_data=G1_Y2S_F1)
         keyboard.add(group_0, group_1)
+        add_keyboard_back_button(keyboard)
+
+        bot.send_message(user_id, text=question, reply_markup=keyboard)
     elif year == Y3S_F2:
         keyboard = types.InlineKeyboardMarkup(row_width=1)  # inline keyboard
         group_0 = types.InlineKeyboardButton(text=G0_Y3S_F2, callback_data=G0_Y3S_F2)
         keyboard.add(group_0)
+        add_keyboard_back_button(keyboard)
+
+        bot.send_message(user_id, text=question, reply_markup=keyboard)
     elif year == Y4S_F2:
         keyboard = types.InlineKeyboardMarkup(row_width=1)  # inline keyboard
         group_0 = types.InlineKeyboardButton(text=G0_Y4S_F2, callback_data=G0_Y4S_F2)
         keyboard.add(group_0)
+        add_keyboard_back_button(keyboard)
+
+        bot.send_message(user_id, text=question, reply_markup=keyboard)
 
 
 def show_menu(user_id, group):
@@ -199,10 +238,8 @@ def show_menu(user_id, group):
     schedule_today = types.InlineKeyboardButton(text='Today', callback_data='schedule today')
     schedule_tomorrow = types.InlineKeyboardButton(text='Tomorrow', callback_data='schedule tomorrow')
     schedule_week = types.InlineKeyboardButton(text='Week', callback_data='schedule week')
-
-    switch_group = types.InlineKeyboardButton(text='choose another group', callback_data='switch group')
-
-    keyboard.add(schedule_today, schedule_tomorrow, schedule_week, switch_group)
+    keyboard.add(schedule_today, schedule_tomorrow, schedule_week)
+    add_keyboard_back_button(keyboard)
 
     question = f'{group} Schedule'
     bot.send_message(user_id, text=question, reply_markup=keyboard)
@@ -225,6 +262,7 @@ def choose_year_and_check_form(user_id, form):
 
         year_4 = types.InlineKeyboardButton(text='4', callback_data=Y4_F0)
         keyboard.add(year_4)
+        add_keyboard_back_button(keyboard)
 
         bot.send_message(user_id, text=question, reply_markup=keyboard)
     elif form == FORM_1:
@@ -234,6 +272,7 @@ def choose_year_and_check_form(user_id, form):
         year_1_spo = types.InlineKeyboardButton(text='1spo', callback_data=Y1S_F1)
         year_2_spo = types.InlineKeyboardButton(text='2spo', callback_data=Y2S_F1)
         keyboard.add(year_1, year_1_spo, year_2_spo)
+        add_keyboard_back_button(keyboard)
 
         bot.send_message(user_id, text=question, reply_markup=keyboard)
     elif form == FORM_2:
@@ -243,6 +282,7 @@ def choose_year_and_check_form(user_id, form):
         year_3_spo = types.InlineKeyboardButton(text='3spo', callback_data=Y3S_F2)
         year_4_spo = types.InlineKeyboardButton(text='4spo', callback_data=Y4S_F2)
         keyboard.add(year_1_spo, year_3_spo, year_4_spo)
+        add_keyboard_back_button(keyboard)
 
         bot.send_message(user_id, text=question, reply_markup=keyboard)
 
