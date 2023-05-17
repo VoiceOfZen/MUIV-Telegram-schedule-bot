4c2daf53fed8fae651a09c79c678f0e99075aed4
Zen <99693461+VoiceOfZen@users.noreply.github.com>
2023-05-17 11:00:06 -0700

    Update exceler.py
    Can extract info now

--- exceler.py
+++ exceler.py
@@ -1,10 +1,45 @@
-import pandas as pd
+import xlrd
 
-df = pd.read_excel(r'schedules\Raspisanie-FIT-ochnaya-f.o-22_23-vesenniy-sem.-IYUN.xls')
+# Open the workbook
+workbook = xlrd.open_workbook('schedules\Raspisanie-FIT-ochnaya-f.o-22_23-vesenniy-sem.-IYUN.xls')
 
-# Search for a specific text in the DataFrame
-search_text = 'your_search_text'
-found_rows = df[df.astype(str).apply(lambda row: row.str.contains(search_text, case=False).any(), axis=1)]
+# Define the sheet indices
+sheet_indices = [0, 1, 2, 3, 4]  # Index 0 corresponds to the first sheet, index 1 to the second sheet, and so on
 
-# Print the found rows
-print(found_rows)
+# Iterate over each sheet index
+for sheet_index in sheet_indices:
+    # Select the sheet by index
+    sheet = workbook.sheet_by_index(sheet_index)
+
+    # Get the number of rows and columns in the sheet
+    num_rows = sheet.nrows
+    num_cols = sheet.ncols
+
+    # Define the column indices for the groups
+    group_columns = [0, 1, 2, 3, 4]  # Adjust these indices as per your column positions
+
+    # Iterate over each group column
+    for col in group_columns:
+        group_info = []
+        # Extract the group information from the column
+        for row in range(num_rows):
+            group_value = sheet.cell_value(row, col)
+            group_info.append(group_value)
+
+        # Normalize the consecutive empty spaces to a maximum of two
+        normalized_group_info = []
+        empty_space_count = 0
+        for info in group_info:
+            if info == "":
+                empty_space_count += 1
+                if empty_space_count <= 2:
+                    normalized_group_info.append(info)
+            else:
+                empty_space_count = 0
+                normalized_group_info.append(info)
+
+        # Print the group information
+        print(f"Sheet Index: {sheet_index}, Column: {col+1}")
+        for info in normalized_group_info:
+            print(info)
+        print()
