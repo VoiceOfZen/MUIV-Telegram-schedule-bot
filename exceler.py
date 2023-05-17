9082864c7642ac9655f5fdaf600e5794b38f54cd
Zen <99693461+VoiceOfZen@users.noreply.github.com>
2023-05-17 10:59:31 -0700

    Update exceler.py
    base

--- exceler.py
+++ exceler.py
@@ -1,1 +1,10 @@
+import pandas as pd
 
+df = pd.read_excel(r'schedules\Raspisanie-FIT-ochnaya-f.o-22_23-vesenniy-sem.-IYUN.xls')
+
+# Search for a specific text in the DataFrame
+search_text = 'your_search_text'
+found_rows = df[df.astype(str).apply(lambda row: row.str.contains(search_text, case=False).any(), axis=1)]
+
+# Print the found rows
+print(found_rows)
